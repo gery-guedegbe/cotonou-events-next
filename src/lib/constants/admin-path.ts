9d76a4b4c4447c6636a01133a0355e033dd903a6
Géry GUEDEGBE <guedegbe2003@gmail.com>
2026-08-10 13:01:59 +0100

    fix : Resolve "Type 'undefined' is not assignable to type 'Url'." issue

--- src/lib/constants/admin-path.ts
+++ src/lib/constants/admin-path.ts
@@ -1,3 +1,4 @@
-export const ADMIN_BASE_PATH = process.env.NEXT_PUBLIC_ADMIN_BASE_PATH;
+export const ADMIN_BASE_PATH =
+  process.env.NEXT_PUBLIC_ADMIN_BASE_PATH ?? "/secure-portal-5d24";
 export const ADMIN_LOGIN_PATH = `${ADMIN_BASE_PATH}/login`;
 export const ADMIN_EVENTS_PATH = `${ADMIN_BASE_PATH}/evenements`;
